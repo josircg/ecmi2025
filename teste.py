5b0d3ab542ba7c930b7145558b261b49882bf157
Josir Cardoso Gomes <josircg@gmail.com>
2025-06-10 15:01:32 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,9 @@
-nome = input('Digite o seu nome')
-if nome.upper() == 'JOSIR':
-    print(f'{nome} é legal!')
-else:
-    print('Eu não te conheço')
+import streamlit as st
+
+st.title('Teste ECMI 2')
+st.write("Tabela")
+#nome = st.input('Digite o seu nome')
+#if nome.upper() == 'JOSIR':
+#    print(f'{nome} é legal!')
+#else:
+#    print('Eu não te conheço')
