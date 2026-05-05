9008821767de64f27c0beae4e14ddfb275de5354
Josir Cardoso Gomes <josircg@gmail.com>
2026-05-05 15:06:43 -0300

    Update test_pandas.py

--- test_pandas.py
+++ test_pandas.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('deputados_2022.csv')
-st.dataframe(df)
+
+sigla = st.text_input('Digite a sigla do partido')
+if sigla:
+    st.dataframe(df[df['sigla']==sigla.upper()])
