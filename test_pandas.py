a15232c3acb31c98c2a2f4742d52b4f8b40de1f2
Josir Cardoso Gomes <josircg@gmail.com>
2026-05-05 15:07:22 -0300

    Update test_pandas.py

--- test_pandas.py
+++ test_pandas.py
@@ -5,4 +5,4 @@ df = pd.read_csv('deputados_2022.csv')
 
 sigla = st.text_input('Digite a sigla do partido')
 if sigla:
-    st.dataframe(df[df['sigla']==sigla.upper()])
+    st.dataframe(df[df['partido']==sigla.upper()])
