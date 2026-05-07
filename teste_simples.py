7c3093d9c5e00a40af7b15265bfdd6fa67d18420
= <=>
2026-05-07 17:40:31 -0300

    vscode

--- teste_simples.py
+++ teste_simples.py
@@ -1,7 +1,16 @@
 import streamlit as st
+from supabase import create_client, Client
 
 st.title('Teste ECMI 2')
 st.write("Tabela")
+
+SUPABASE_URL = "https://fdpjesdfiwnuxuabvrdn.supabase.co"
+SUPABASE_KEY = st.secrets["supabase"]
+connection: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+st.write("Sucesso: Cliente Supabase inicializado!")
+resposta = connection.table("django_migrations").select("*").limit(1).execute()
+st.write("Dados recebidos:", resposta.data)
+
 # print('xuxu')
 # nome = st.input('Digite o seu nome')
 #if nome.upper() == 'JOSIR':
